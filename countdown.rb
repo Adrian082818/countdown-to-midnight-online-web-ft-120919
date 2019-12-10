14ce062cae171b162dc01f023cc01017b7ae65c4
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-10 22:23:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,8 +9,8 @@ end
 
 def countdown_with_sleep(run_time = 5)
   while run_time == 5 
-  puts "#{number} SECOND(S)!"
-  number -= 1 
-  sleep(5)
+  puts "#{run_time} SECOND(S)!"
+  run_time -= 1 
+  sleep(1)
 end 
 end 
